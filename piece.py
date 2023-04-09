75bf7eba36bbcd65859c62cd60497594045109e8
Matěj Ježek <jetam.jezek@gmail.com>
2023-04-09 22:37:40 +0200

    do piece pridana metoda k vykresleni

--- piece.py
+++ piece.py
@@ -1,9 +1,19 @@
 # třída kámen
+import pygame
+
 class Piece:
-    def __init__(self, color, location) -> None:
+    def __init__(self, window, color, location) -> None:
+        self.window = window
         self.color = color
         self.location = location # ještě nevím jak budeme určovat řadu a pozici
+        self.size = 35 # poloměr kamene na hrací ploše (px)
         self.in_house = False # zda se nachází v domečku
         self.is_captured = False # zda je vyhozený
         self.is_movable = False # jde s ním momentálně hýbat?
         self.memory = [] # historie
+        self.outline_color = (55, 55, 55)
+
+    def draw(self):
+        pygame.draw.circle(self.window, self.outline_color, None, self.size+2)
+        pygame.draw.circle(self.window, self.color, None, self.size)
+        #                                           ^ souřadnice středu DOPLNIT!!
