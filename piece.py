94eb69427d2c537a300158554fd178bba31e994e
Matěj Ježek <jetam.jezek@gmail.com>
2023-04-09 21:09:31 +0200

    trida kamen

--- piece.py
+++ piece.py
@@ -1,3 +1,9 @@
+# třída kámen
 class Piece:
-    def __init__(self,) -> None:
-        pass
+    def __init__(self, color, location) -> None:
+        self.color = color
+        self.location = location # ještě nevím jak budeme určovat řadu a pozici
+        self.in_house = False # zda se nachází v domečku
+        self.is_captured = False # zda je vyhozený
+        self.is_movable = False # jde s ním momentálně hýbat?
+        self.memory = [] # historie
