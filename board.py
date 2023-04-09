b45366f65ef553e8d010027fb9a818782de89caa
Matěj Ježek <jetam.jezek@gmail.com>
2023-04-09 20:08:15 +0200

    rosireny vyhazovaci prostor a pridany domecky

--- board.py
+++ board.py
@@ -1,8 +1,9 @@
 import pygame
 pygame.init
 boardColor = (133,100,55)
-size = (1200,900)
+size = (1400,900)
 corners = (66,30,6)
+darker_corners = (42,19,4)
 bla = (0,0,0)
 whi = (255,255,255)
 screen = pygame.display.set_mode(size)
@@ -16,9 +17,12 @@ while done == False:
 
     screen.fill(boardColor)
     #krajní linie
-    pygame.draw.line(screen,corners,(602,0),(602,900),40)
+    pygame.draw.line(screen,corners,(627,0),(627,900),90)
     pygame.draw.line(screen,corners,(0,0),(0,900),30)
-    pygame.draw.line(screen,corners,(1200,0),(1200,900),30)
+    pygame.draw.polygon(screen,corners,[(1240,0),(1400,0),(1400,900),(1240,900)],0)
+    #domečky
+    pygame.draw.polygon(screen, darker_corners, [(1275,100), (1365,100), (1365,350), (1275,350)],0)
+    pygame.draw.polygon(screen, darker_corners, [(1275,550), (1365,550), (1365,800), (1275,800)],0)
     #triangle horní levé
     pygame.draw.polygon(screen,whi,[(62.5, 350), (105, 0), (20, 0)],0)
     pygame.draw.polygon(screen,bla,[(157.5, 350), (200, 0), (115, 0)],0)
@@ -34,19 +38,19 @@ while done == False:
     pygame.draw.polygon(screen,bla,[(442.5, 550), (485, 900), (400, 900)],0)
     pygame.draw.polygon(screen,whi,[(537.5, 550), (580, 900), (495, 900)],0)
     #triangle horní pravé
-    pygame.draw.polygon(screen,whi,[(667.5, 350), (710, 0), (625, 0)],0)
-    pygame.draw.polygon(screen,bla,[(762.5, 350), (805, 0), (720, 0)],0)
-    pygame.draw.polygon(screen,whi,[(857.5, 350), (900, 0), (815, 0)],0)
-    pygame.draw.polygon(screen,bla,[(952.5, 350), (995, 0), (910, 0)],0)
-    pygame.draw.polygon(screen,whi,[(1047.5, 350), (1090, 0), (1005, 0)],0)
-    pygame.draw.polygon(screen,bla,[(1132.5, 350), (1182.5, 0), (1100, 0)],0)
+    pygame.draw.polygon(screen,whi,[(717.5, 350), (760, 0), (675, 0)],0)
+    pygame.draw.polygon(screen,bla,[(812.5, 350), (855, 0), (770, 0)],0)
+    pygame.draw.polygon(screen,whi,[(907.5, 350), (950, 0), (865, 0)],0)
+    pygame.draw.polygon(screen,bla,[(1002.5, 350), (1045, 0), (960, 0)],0)
+    pygame.draw.polygon(screen,whi,[(1097.5, 350), (1140, 0), (1055, 0)],0)
+    pygame.draw.polygon(screen,bla,[(1192.5, 350), (1235, 0), (1150, 0)],0)
     #triangle dolní pravé
-    pygame.draw.polygon(screen,bla,[(667.5, 550), (710, 900), (625, 900)],0)
-    pygame.draw.polygon(screen,whi,[(762.5, 550), (805, 900), (720, 900)],0)
-    pygame.draw.polygon(screen,bla,[(857.5, 550), (900, 900), (815, 900)],0)
-    pygame.draw.polygon(screen,whi,[(952.5, 550), (995, 900), (910, 900)],0)
-    pygame.draw.polygon(screen,bla,[(1047.5, 550), (1090, 900), (1005, 900)],0)
-    pygame.draw.polygon(screen,whi,[(1132.5, 550), (1182.5, 900), (1100, 900)],0)
+    pygame.draw.polygon(screen,bla,[(717.5, 550), (760, 900), (675, 900)],0)
+    pygame.draw.polygon(screen,whi,[(812.5, 550), (855, 900), (770, 900)],0)
+    pygame.draw.polygon(screen,bla,[(907.5, 550), (950, 900), (865, 900)],0)
+    pygame.draw.polygon(screen,whi,[(1002.5, 550), (1045, 900), (960, 900)],0)
+    pygame.draw.polygon(screen,bla,[(1097.5, 550), (1140, 900), (1055, 900)],0)
+    pygame.draw.polygon(screen,whi,[(1192.5, 550), (1235, 900), (1150, 900)],0)
 
     pygame.display.flip()
     clock.tick(60)
