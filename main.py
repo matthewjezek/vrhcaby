e64ae94a790a2bec694a28e60208d945495d61e9
Matěj Ježek <jetam.jezek@gmail.com>
2023-04-19 21:25:48 +0200

    Implementace menu před startem hry, třída tlačítka, přiložený font z Google Fonts s open licencí.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import board
 import dice
+from menu import Button 
 pygame.init()
 
 def main() -> None:
@@ -16,15 +17,30 @@ def main() -> None:
     pygame.display.set_caption("My game")
     clock = pygame.time.Clock()
     done = False
+    game_started = False
     
+    # hlavní loop
     while done == False:
+        # sběr eventů
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
-        
-        main_board = board.Board(screen, clock, colors, size)
-        main_board.make_board()
-        clock.tick(60)
+
+        # vyvolání hrací plochy / menu
+        if game_started:
+            main_board = board.Board(screen, clock, colors, size)
+            main_board.make_board()
+            clock.tick(60)
+        else:
+            pygame.display.set_caption("Main menu")
+            screen.fill(colors['board_color'])
+            start = Button(screen, 300, 112.5, colors['bla'], 'START')
+            if start.draw((700,350)):
+                game_started = True
+            quit = Button(screen, 200, 112.5, colors['bla'], 'QUIT')
+            if quit.draw((700, 550)):
+                done = True
+            pygame.display.flip()
     pygame.quit()
 
 if __name__ == '__main__':
