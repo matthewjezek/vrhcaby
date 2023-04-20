2915abe3580f8a85b03f7bace6976f678d40e5ed
Matěj Ježek <jetam.jezek@gmail.com>
2023-04-20 18:49:37 +0200

    oprava pristupu k souborum

--- dice.py
+++ dice.py
@@ -1,13 +1,14 @@
 import random
 from typing import Iterator
 import pygame as pg
+import os
 
-side_1 = pg.image.load("Dice/Side_1_Pip.png", "Side 1")
-side_2 = pg.image.load("Dice/Side_2_Pips.png", "Side 2")
-side_3 = pg.image.load("Dice/Side_3_Pips.png", "Side 3")
-side_4 = pg.image.load("Dice/Side_4_Pips.png", "Side 4")
-side_5 = pg.image.load("Dice/Side_5_Pips.png", "Side 5")
-side_6 = pg.image.load("Dice/Side_6_Pips.png", "Side 6")
+side_1 = pg.image.load(os.path.join("Side_1_Pip.png"))
+side_2 = pg.image.load(os.path.join("Side_2_Pips.png"))
+side_3 = pg.image.load(os.path.join("Side_3_Pips.png"))
+side_4 = pg.image.load(os.path.join("Side_4_Pips.png"))
+side_5 = pg.image.load(os.path.join("Side_5_Pips.png"))
+side_6 = pg.image.load(os.path.join("Side_6_Pips.png"))
 
 class Dice:
     def __init__(self, sides=6) -> None:
