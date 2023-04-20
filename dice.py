3ad5bcee63c0b057d3f1018e0f76cd442d67546e
Matěj Ježek <jetam.jezek@gmail.com>
2023-04-20 18:33:47 +0200

    kostka strany

--- dice.py
+++ dice.py
@@ -1,6 +1,13 @@
 import random
 from typing import Iterator
+import pygame as pg
 
+side_1 = pg.image.load("Dice/Side_1_Pip.png", "Side 1")
+side_2 = pg.image.load("Dice/Side_2_Pips.png", "Side 2")
+side_3 = pg.image.load("Dice/Side_3_Pips.png", "Side 3")
+side_4 = pg.image.load("Dice/Side_4_Pips.png", "Side 4")
+side_5 = pg.image.load("Dice/Side_5_Pips.png", "Side 5")
+side_6 = pg.image.load("Dice/Side_6_Pips.png", "Side 6")
 
 class Dice:
     def __init__(self, sides=6) -> None:
