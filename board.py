6f251007c38308a3f9ae04eb41fcb890b1bd80d0
Josef Nesnídal <josefnesnidal@email.cz>
2023-04-30 00:31:11 +0200

    nově tÅklása player v player.py

--- board.py
+++ board.py
@@ -1,5 +1,3 @@
-
-
 class Place:
     def __init__(self, stack):
         self.stack = stack
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import board
 import dice
+import player
 
 board = board.Board()
 dice = dice.Dice()
+AI = player.Player("AI", "K", "AI")
 
 board.show()
 dice.roll()
 dice.show()
 moves = dice.generate_moves()
 print(moves)
-
 
