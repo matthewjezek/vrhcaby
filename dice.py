c7ae4a6f3f5b6f748838899c2a170af37715229c
Matěj Ježek <jetam.jezek@gmail.com>
2023-04-29 23:57:28 +0200

    zjednodusena kostka a predelana na dvojkostku

--- dice.py
+++ dice.py
@@ -1,5 +1,6 @@
 import random
 from typing import Iterator
+from numpy import roll
 import pygame as pg
 import os
 
@@ -11,34 +12,31 @@ side_5 = pg.image.load(os.path.join("IMG/Side_5_Pips.png"))
 side_6 = pg.image.load(os.path.join("IMG/Side_6_Pips.png"))
 
 class Dice:
-    def __init__(self, sides=6) -> None:
-        self.sides = sides # počet stran, defaultně 6
+    def __init__(self) -> None:
         self.rolls = [] # historie hodů
-        self.total = 0 # celkový počet hodů
-        self.total_rolls = 0 # součet všech hodů
+        self.total_rolls = 0 # celkový počet hodů
     
     def roll(self) -> int:
-        roll = random.randint(1, self.sides)
-        self.rolls.append(roll)
-        self.total += roll
+        roll1 = random.randint(1, 6)
+        roll2 = random.randint(1, 6)
         self.total_rolls += 1
-        return roll
+        if roll1 == roll2:
+            self.rolls.append((roll1, roll1, roll1, roll1))
+            roll = roll1 * 4
+            return roll
+        else:
+            self.rolls.append((roll1, roll2))
+            roll = roll1 + roll2
+            return roll
     
-    def __repr__(self) -> str:
-        return f'Dice[rolls={self.rolls},total_rolls={self.total_rolls},total={self.total}]'
+    def __repr__(self) -> str: # nějaké insider info
+        return f'Dice[rolls={self.rolls},total_rolls={self.total_rolls}]'
 
-    def __str__(self) -> str:
-        return f'Hozeno {self.total}'
+    def __str__(self) -> str: # vypíše poslední hozenou dvojici
+        return f'Hozeno {self.rolls[-1]}'
     
-    def __len__(self) -> int:
-        return self.sides
-    
-    def __add__(self, value: int):
-        self.total = self.total + value
-        return self
-
-    def __contains__(self, value: int) -> bool:
-        return value in self.rolls
+    def __len__(self) -> int: # vrátí počet hodů
+        return self.total_rolls
     
     def __iter__(self) -> Iterator: # list(), for cyklus
         return iter(self.rolls)
