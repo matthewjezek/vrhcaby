47012a765bb74aade4889b3050f8dfc9d956aa9d
Josef Nesnídal <josefnesnidal@email.cz>
2023-04-30 17:23:00 +0200

    Přehlednější board.show pro testování - hotovo

--- board_layers.py
+++ board_layers.py
@@ -1,7 +1,7 @@
 
 def layers(board):
-    max_num = len(max(board.make_stacks_list())) + 1
-    for j in range(1, max_num + 1):
+    max_num = len(max(board.make_stacks_list()))
+    for j in range(1, max_num + 2):
         layer = " "
         i = 11
         for _ in range(0, 12):
@@ -12,7 +12,8 @@ def layers(board):
                 layer += "   "
             i -= 1
         print(f"|{layer}|")
-    for k in range(1, max_num):
+    k = max_num - 1
+    for _ in range(1, max_num):
         layer = " "
         for l in range(12, 24):
             place = board.make_stacks_list()[l]
@@ -21,4 +22,5 @@ def layers(board):
             else:
                 layer += "   "
         print(f"|{layer}|")
+        k -= 1
 
