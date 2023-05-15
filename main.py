84828e49f35b4e7e32d755ef210ee540a3afa2e1
Matěj Ježek <jetam.jezek@gmail.com>
2023-05-15 10:56:07 +0200

    sjednoceni s boardem

--- main.py
+++ main.py
@@ -25,7 +25,10 @@ def game_loop() -> None:
             pygame.display.set_caption("Vrhcaby")
             main_board = board.Board(screen, clock, colors, size)
             main_board.make_board()
-            clock.tick(60)
+
+            roll = Button(screen, 200, 112.5, colors['bla'], 'ROLL')
+            if roll.draw((size[0]*0.449, size[1]*0.5)):
+                pass
         else:
             pygame.display.set_caption("Main menu")
             screen.fill(colors['board_color'])
@@ -35,6 +38,8 @@ def game_loop() -> None:
             quit = Button(screen, 200, 112.5, colors['bla'], 'QUIT')
             if quit.draw((size[0]//2, size[1]*0.6111)):
                 done = True
+
+        clock.tick(60)
         pygame.display.flip()
     pygame.quit()
 #přidat game_loop() pro spuštění
